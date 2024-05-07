472812b70e567f94cb15b672e4d0a8c5a3f53b38
amineirb <contact@irbaine.com>
2024-05-07 11:15:46 -0700

    Done

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+s module provides a function that sends a get request to the Reddit API
+    with the specified subreddit then process the response and prints the
+        titles of the first 10 hot posts listed for a given subreddit
+        """
+
+
+        def top_ten(subreddit):
+            """
+                top_ten: This function queries the Reddit API
+                    Args:
+                                subreddit (str): The api subreddit to send a get request to.
+                                    Return: None
+                                        """
+
+                                            import requests
+
+                                                url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+                                                    params = {'limit': 10}
+                                                        headers = {'User-Agent': 'MyServer/1.0'}
+
+                                                            response = requests.get(url, headers=headers, params=params)
+                                                                if response.status_code != 200:
+                                                                        print(None)
+                                                                                return
+                                                                                    try:
+                                                                                            children = response.json()['data']['children']
+                                                                                                    for child in children:
+                                                                                                                print(child['data'].get('title'))
+                                                                                                                    except IndexError:
+                                                                                                                            print(None)
